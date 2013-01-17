8099ae0a2ccb4e9a777bd1a2b99903729cfb1153
roger.dargaville <roger.dargaville@gmail.com>
2013-01-17 00:46:24 +0000

    Buggered around with a few things

--- runmulti.py
+++ runmulti.py
@@ -30,36 +30,52 @@ import pickle
 # config files, to run with asst5_config.txt, and
 # then runs them.
 
-solar_cost_list = [1, 2, 3]
+#solar_cost_list = [1, 2, 3]
+year_list = [2013,2020,2030,2040,2050]
 
 # Create the config files
-for solar_cost in solar_cost_list:
-    filename = 'solar_{:d}_config.txt'.format(solar_cost)
+#for solar_cost in solar_cost_list:
+#    filename = 'solar_{:d}_config.txt'.format(solar_cost)
+#    f = open(filename, 'w')
+#    f.write('[Solar]\n')
+#    f.write('capex: {:d}\n'.format(solar_cost))
+#    f.write('[Master]\n')
+#    f.write('output_file: solar_{:d}.pkl\n'.format(solar_cost))
+#    f.close()
+
+for runyear in year_list:
+    filename = 'runyear_{:d}_config.txt'.format(runyear)
     f = open(filename, 'w')
-    f.write('[Solar]\n')
-    f.write('capex: {:d}\n'.format(solar_cost))
+    f.write('[Algorithm]\n')
+    f.write('runyear: {:d}\n'.format(runyear))
     f.write('[Master]\n')
-    f.write('output_file: solar_{:d}.pkl\n'.format(solar_cost))
+    f.write('output_file: runyear_{:d}.pkl\n'.format(runyear))
     f.close()
 
 # Run the sims
-for solar_cost in solar_cost_list:
-    filename = 'solar_{:d}_config.txt'.format(solar_cost)
-    logname = 'solar_{:d}.log'.format(solar_cost)
-    runmureil.runmureil(['-f', 'asst5_config.txt', '-f', filename, '-l', logname,
-        '--iterations', '100'])
+#for solar_cost in solar_cost_list:
+#    filename = 'solar_{:d}_config.txt'.format(solar_cost)
+#    logname = 'solar_{:d}.log'.format(solar_cost)
+#    runmureil.runmureil(['-f', 'GEconfig.txt', '-f', filename, '-l', logname,
+#        '--iterations', '1'])
+
+for runyear in year_list:
+    filename = 'runyear_{:d}_config.txt'.format(runyear)
+    logname = 'runyear_{:d}.log'.format(runyear)
+    runmureil.runmureil(['-f', 'GEconfig.txt', '-f', filename, '-l', logname,
+        '--iterations', '1'])
 
 # Collect the results
-for solar_cost in solar_cost_list:
-    pkl_name = 'solar_{:d}.pkl'.format(solar_cost)
-    p = pickle.load(open(pkl_name, 'rb'))
-    costs = p['best_results']['cost']
-    total_cost = sum([pair[1] for pair in costs])
-    capacities = p['best_results']['capacity']
-    
-    print '=========================================='
-    print 'Solar Capex = {:d}'.format(solar_cost)
-    print 'Total Cost $M = {:.2f}'.format(total_cost)
-    print 'Capacities:'
-    print capacities
+#for runyear in year_list:
+#    pkl_name = 'runyear_{:d}.pkl'.format(runyear)
+#    p = pickle.load(open(pkl_name, 'rb'))
+#    costs = p['best_results']['cost']
+#    total_cost = sum([pair[1] for pair in costs])
+#    capacities = p['best_results']['capacity']
+#    
+#    print '=========================================='
+#    print 'Year = {:d}'.format(runyear)
+#    print 'Total Cost $M = {:.2f}'.format(total_cost)
+#    print 'Capacities:'
+#    print capacities
     
